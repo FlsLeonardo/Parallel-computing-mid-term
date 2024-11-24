1c4009f2371b989179b7ab70cca4353ef68badda
Falsarolo Leonardo <18617@studenti.marconiverona.edu.it>
2024-11-24 13:51:22 +0100

    fixed compile bug added more compiler option to compile and option to choose  several compile option together,added detiled Readme

--- method3/bin/compile.sh
+++ method3/bin/compile.sh
@@ -17,7 +17,6 @@ if [ $# -eq 0 ]; then
   ./Main 11 "O1"
   ./Main 12 "O1"
   
-  
   g++-9.1.0 -c main.cpp -fopenmp
   g++-9.1.0 -c serialTransposition.cpp
   g++-9.1.0 -c implicitTransposition.cpp -O2
@@ -37,7 +36,7 @@ if [ $# -eq 0 ]; then
   rm ../output/Omp.csv
   g++-9.1.0 -c main.cpp -fopenmp
   g++-9.1.0 -c serialTransposition.cpp
-  g++-9.1.0 -c implicitTransposition.cpp -O3 
+  g++-9.1.0 -c implicitTransposition.cpp -O3
   g++-9.1.0 -c ompTransposition.cpp -fopenmp
   g++-9.1.0 main.o serialTransposition.o implicitTransposition.o ompTransposition.o -o Main -fopenmp
   ./Main 4 "O3"
@@ -51,37 +50,49 @@ if [ $# -eq 0 ]; then
   ./Main 12 "O3"
 fi
 
+# Se viene passato un solo parametro (O1, O2, etc.)
 if [ $# -eq 1 ]; then
     module load gcc91
     g++-9.1.0 -c main.cpp -fopenmp
     g++-9.1.0 -c serialTransposition.cpp
-    g++-9.1.0 -c implicitTransposition.cpp -$1
+    
+    # Se il parametro contiene uno spazio, lo dividiamo nei flag
+    OPTS=($1)  # Usa un array per gestire i flag separati
+    
+    # Compilazione con i flag separati
+    g++-9.1.0 -c implicitTransposition.cpp -"${OPTS[@]}"  # Passa i flag separati
     g++-9.1.0 -c ompTransposition.cpp -fopenmp
     g++-9.1.0 main.o serialTransposition.o implicitTransposition.o ompTransposition.o -o Main -fopenmp
-    echo $1
-   ./Main 4 $1
-   ./Main 5 $1
-   ./Main 6 $1
-   ./Main 7 $1
-   ./Main 8 $1
-   ./Main 9 $1
-   ./Main 10 $1
-   ./Main 11 $1
-   ./Main 12 $1
+    
+    echo "Compilazione con i seguenti flag: ${OPTS[@]}"  # Mostra i flag usati
+    
+    # Passiamo la stringa completa (concatenata) al programma Main
+    FLAGS="$1"  # La stringa passata da concatenare
+    ./Main 4 "$FLAGS"
+    ./Main 5 "$FLAGS"
+    ./Main 6 "$FLAGS"
+    ./Main 7 "$FLAGS"
+    ./Main 8 "$FLAGS"
+    ./Main 9 "$FLAGS"
+    ./Main 10 "$FLAGS"
+    ./Main 11 "$FLAGS"
+    ./Main 12 "$FLAGS"
 fi
 
-
-
+# Se vengono passati due parametri
 if [ $# -eq 2 ]; then
   # Compilazione dei file
   module load gcc91
   g++-9.1.0 -c main.cpp -fopenmp
   g++-9.1.0 -c serialTransposition.cpp
-  g++-9.1.0 -c implicitTransposition.cpp -$1
+  
+  OPTS=($1)  # Usa un array per gestire i flag separati
+  g++-9.1.0 -c implicitTransposition.cpp -"${OPTS[@]}"  # Passa i flag separati
   g++-9.1.0 -c ompTransposition.cpp -fopenmp
   
   # Link dei file oggetto
   g++-9.1.0 main.o serialTransposition.o implicitTransposition.o ompTransposition.o -o Main -fopenmp
   # Eseguiamo il programma col parametro
-  ./Main $2 $1
+  FLAGS="$1"  # La stringa passata da concatenare
+  ./Main $2 "$FLAGS"  # Passa i flag concatenati al programma Main
 fi
